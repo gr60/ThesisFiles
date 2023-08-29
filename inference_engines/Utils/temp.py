e5aca24c3fc306eb768121da1e944ec272083ec8
gr60 <gr60@st-andrews.ac.uk>
2023-08-29 12:33:57 -0300

    Thesis files

--- inference_engines/Utils/temp.py
+++
@@ -1,6 +0,0 @@
-data = [{'A':'B'},{'A':'C'},{'A':'D'},{'A':'E'}]
-rule = ''
-for alternative in data:
-    for drugA, drugB in alternative.items():
-        rule = f'((Xor({drugA} Or {drugB})),{rule})'
-        print (rule)
